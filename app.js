98622ce3ac855d270aa16898eaf73bd522493dde
Adam Jean-Laurent <ajean-laurent@my.uri.edu>
2019-05-06 03:19:10 -0400

    Added parsing For Data

--- app.js
+++ app.js
@@ -2,9 +2,14 @@ const express = require('express');
 const request = require('request');
 const bodyParser = require('body-parser');
 const zipCodes = require('zipcodes');
-const port = 3000;
+const unixTime = require('./public/unixFunctions.js');
 const app = express();
 const apiKey = "f06d69d89a74e576af8825f26319c6c8";
+const port = 3000;
+
+let firstDayTemps = [];
+let secondDayTemps = [];
+let thirdDayTemps = [];
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
@@ -20,7 +25,7 @@ app.post("/",function(req,res){
             return res.redirect("/");
     }
     let options = {
-        url: `http://api.openweathermap.org/data/2.5/weather?zip=${req.body.zipcode},us&units=imperial&appid=` + apiKey,
+        url: `https://api.openweathermap.org/data/2.5/forecast?zip=${req.body.zipcode},us&units=Imperial&appid=` + apiKey,
         method: "POST"
     }
 
@@ -30,8 +35,48 @@ app.post("/",function(req,res){
         }
         if(!error){
             let data = JSON.parse(body);
-            console.log("It Is Currently " + data.main.temp +" Degrees Fahrenheit!");
+            for(let i = 0; i < data.list.length; i++){
+                let unixStamp = data.list[i].dt;
+                console.log(unixTime.convertUnixToDayMonth(unixStamp) + " " +  unixTime.convertUnixToAMPM(unixStamp) + " " + data.list[i].main.temp);
+            }
+            for(let i = 0; i < data.list.length; i++){
+                if(unixTime.convertUnixToAMPM(data.list[i].dt) === '11:00 PM'){
+                    var startIndex = i + 1;
+                    break;
+                }
+            }
+            console.log(startIndex);
+            for(let i = 0; i < 8; i++){
+                firstDayTemps.push(data.list[startIndex + i].main.temp);
+            }
+
+            for(let i = 0; i < 8; i++){
+                secondDayTemps.push(data.list[startIndex + i + 8].main.temp);
+            }
+
+            for(let i = 0; i < 8; i++){
+                thirdDayTemps.push(data.list[startIndex + i + 16].main.temp);
+            }
+            console.log(firstDayTemps);
+            console.log(secondDayTemps);
+            console.log(thirdDayTemps);
+
+            let currentDate = unixTime.convertUnixToDayMonth(data.list[0].dt);
+            let firstDayDate = unixTime.convertUnixToDayMonth(data.list[startIndex].dt);
+            let secondDayDate = unixTime.convertUnixToDayMonth(data.list[startIndex + 8].dt);
+            let thirdDayDate = unixTime.convertUnixToDayMonth(data.list[startIndex + 16].dt);
+
+            res.render("dashboard", {firstDayTemps: firstDayTemps, 
+                secondDayTemps: secondDayTemps, 
+                thirdDayTemps: thirdDayTemps,
+                currentTemp: data.list[0].main.temp,
+                currentDate: currentDate,
+                firstDayDate: firstDayDate,
+                secondDayDate: secondDayDate,
+                thirdDayDate: thirdDayDate
+            });
         }
+
     });
 });
 
@@ -43,8 +88,12 @@ app.listen(port,function(){
 //api key  for youtube api AIzaSyDrleh-H310BeSbiMZ5lem4L_o3XZ4LYHI
 
 
-//https://api.openweathermap.org/data/2.5/forecast?zip=02919,us&appid=f06d69d89a74e576af8825f26319c6c8
+//https://api.openweathermap.org/data/2.5/forecast?zip=02919,us&units=imperia&appid=f06d69d89a74e576af8825f26319c6c8
 //forecast api call
 
 
 //UTC is 4 hours later than EST
+
+// https://api.openweathermap.org/data/2.5/forecast/daily?zip=02919&appid=f06d69d89a74e576af8825f26319c6c8
+
+
--- public/unixFunctions.js
+++ public/unixFunctions.js
@@ -1,38 +1,30 @@
-// returns the date from unix time stamp to May-5-2019 18:16:55 format
-exports.convertUnixToDate = function(unixtimestamp){
-    // Months array
+// Display date time in MM-dd-yyyy h:m:s format
+exports.convertUnixToMMDDYYYY = function(unixTimeStamp){
     let months_arr = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
     // Convert timestamp to milliseconds
-    let date = new Date(unixtimestamp*1000);
-    // Year
+    let date = new Date(unixTimeStamp*1000);
     let year = date.getFullYear();
-    // Month
     let month = months_arr[date.getMonth()];
-    // Day
     let day = date.getDate();
-    // Hours
     let hours = date.getHours();
-    // Minutes
     let minutes = "0" + date.getMinutes();
-    // Seconds
     let seconds = "0" + date.getSeconds();
-    // Display date time in MM-dd-yyyy h:m:s format
-    let convdataTime = month+'-'+day+'-'+year+' '+hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
-    return convdataTime;
+    let convertTime = month+'-'+day+'-'+year+' '+hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
+    return convertTime;
    }
 
 //returns the date in 5 May format
-exports.getMonthFromUnix = function(unixtimestamp){
+exports.convertUnixToDayMonth = function(unixTimeStamp){
     let months_arr = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
-    let date = new Date(unixtimestamp*1000);
+    let date = new Date(unixTimeStamp*1000);
     let month = months_arr[date.getMonth()];
     let day = date.getDate();
   return day + " " + month;
   }
 
 //returns the time in AM/PM Format in EST
-  exports.timeFromUnix = function(unixtimestamp){
-    let date = new Date(unixtimestamp*1000);
+  exports.convertUnixToAMPM = function(unixTimeStamp){
+    let date = new Date(unixTimeStamp*1000);
     let hours = date.getHours();
     let minutes = date.getMinutes();
     if(minutes < 10){
