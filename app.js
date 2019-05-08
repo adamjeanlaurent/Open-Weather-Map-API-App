c0d896bc157fa9dfce2748636c134c50361fe72c
Adam Jean-Laurent <ajean-laurent@my.uri.edu>
2019-05-08 19:18:05 -0400

    Finishing Touches

--- app.js
+++ app.js
@@ -17,11 +17,11 @@ app.use(express.static("public"));
 app.set("view engine", "ejs");      
 
 app.get("/", function(req,res){
-    // res.sendFile(__dirname + "/index.html");
     res.render("index", {wrongZip: false});
 });
 
 app.post("/",function(req,res){     
+    // Check To See If The User Inputed Zip-Code is Valid
     let zipCode = zipCodes.lookup(req.body.zipcode);
         if(zipCode === undefined){
             return res.render("index", {wrongZip: true});
@@ -30,19 +30,25 @@ app.post("/",function(req,res){
         url: `https://api.openweathermap.org/data/2.5/forecast?zip=${req.body.zipcode},us&units=Imperial&appid=` + apiKey,
         method: "POST"
     }
-
+    // Makes API Call To openweathermap's API
     request(options,function(error,response,body){
+        // Sends The User Back To The Index Page If There Is An Error With The API Response
         if(error){
             console.log(respnose.statusCode);
+            return res.render("index", {wrongZip: false});
         }
         if(!error){
             let data = JSON.parse(body);
             for(let i = 0; i < data.list.length; i++){
+                /* Because The API's Response Is Different Depending On The Time Of The Call, 
+                This Loops Finds When The Day After The Current Day Starts
+                */
                 if(unixTime.convertUnixToAMPM(data.list[i].dt) === '11:00 PM'){
                     var startIndex = i + 1;
                     break;
                 }
             }
+            // Storing The Temperatures Of The Next Three Days
             for(let i = 0; i < 8; i++){
                 firstDayTemps.push(data.list[startIndex + i].main.temp);
             }
@@ -55,11 +61,13 @@ app.post("/",function(req,res){
                 thirdDayTemps.push(data.list[startIndex + i + 16].main.temp);
             }
 
+            // Gets The Date Of Current And Following Three Days
             let currentDate = unixTime.convertUnixToDayMonth(data.list[0].dt);
             let firstDayDate = unixTime.convertUnixToDayMonth(data.list[startIndex].dt);
             let secondDayDate = unixTime.convertUnixToDayMonth(data.list[startIndex + 8].dt);
             let thirdDayDate = unixTime.convertUnixToDayMonth(data.list[startIndex + 16].dt);
 
+            // Renders With Data For EJS
             res.render("dashboard", {firstDayTemps: firstDayTemps, 
                 secondDayTemps: secondDayTemps, 
                 thirdDayTemps: thirdDayTemps,
